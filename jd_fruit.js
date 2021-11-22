71fcfe6d3b6d71ea59755c94a579c755562660a6
farmaster <2422263284@qq.com>
2021-11-22 19:20:09 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -925,9 +925,9 @@ async function receiveFriendInvite() {
       continue
     }
     await inviteFriend(code);
-   console.log(`接收邀请成为好友结果:${JSON.stringify($.inviteFriendRes)}`)
+//   console.log(`接收邀请成为好友结果:${JSON.stringify($.inviteFriendRes)}`)
     if ($.inviteFriendRes && $.inviteFriendRes.helpResult && $.inviteFriendRes.helpResult.code === '0') {
-   //   console.log(`接收邀请成为好友结果成功,您已成为${$.inviteFriendRes.helpResult.masterUserInfo.nickName}的好友`)
+     console.log(`接收邀请成为好友结果成功,您已成为${$.inviteFriendRes.helpResult.masterUserInfo.nickName}的好友`)
     } else if ($.inviteFriendRes && $.inviteFriendRes.helpResult && $.inviteFriendRes.helpResult.code === '17') {
     //  console.log(`接收邀请成为好友结果失败,对方已是您的好友`)
     }
